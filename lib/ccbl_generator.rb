3b44cc109b4fbf1f0eadea87ae21ba05c3aec3e6
yalab <rudeboyjet@gmail.com>
2014-03-18 16:03:51 +0900

    generate custom_class files.

--- lib/ccbl_generator.rb
+++ lib/ccbl_generator.rb
@@ -9,6 +9,8 @@ class TemplateRenderer < Erubis::Eruby
 end
 
 class CcblGenerator
+  attr_accessor :custom_classes
+
   def initialize(project_name: ,path: )
     raise "No such plist file #{path}" unless File.exists?(path)
 
@@ -17,13 +19,19 @@ class CcblGenerator
     if class_name.length < 1
       class_name = File.basename(path, ".ccb")
     end
+    @custom_classes = plist["nodeGraph"]["children"]
+      .select{|n| n['baseClass'] != "CCBFile" && n['customClass'].length > 0 }
+      .map{|n| n['customClass'] }
+    @custom_classes.uniq!
+
     member_variables =  plist["nodeGraph"]['children'].select{|n| n['memberVarAssignmentName'].length > 0 }
     custom_properties = plist["nodeGraph"]["customProperties"]
-    @binding = {project_name: project_name,
-                class_name:   class_name,
-                plist:        plist,
-                member_variables: member_variables,
+    @binding = {project_name:      project_name,
+                class_name:        class_name,
+                plist:             plist,
+                member_variables:  member_variables,
                 custom_properties: custom_properties,
+                custom_classes:    custom_classes,
                 property_type: {0 => 'int', 1 => 'float', 2 => 'bool', 3 => 'string' },
                 symbol_type:   {0 => '%d',  1 => '%f',    2 => '%d',   3 => '%s'}
     }
@@ -31,23 +39,34 @@ class CcblGenerator
   end
 
   def generate
-    {loader: generate_loader,
-     header: generate_header,
-     body: generate_body}
+    {loader: generate_loader(@binding),
+     header: generate_header(@binding),
+     body:   generate_body(@binding)}
   end
 
-  def generate_loader
+  def generate_loader(binding)
     template = File.expand_path("../../template/loader.h.erb", __FILE__)
-    @loader = TemplateRenderer.new(File.read(template)).result(@binding)
+    TemplateRenderer.new(File.read(template)).result(binding)
   end
 
-  def generate_header
+  def generate_header(binding)
     template = File.expand_path("../../template/header.h.erb", __FILE__)
-    @loader = TemplateRenderer.new(File.read(template)).result(@binding)
+    TemplateRenderer.new(File.read(template)).result(binding)
   end
 
-  def generate_body
+  def generate_body(binding)
     template = File.expand_path("../../template/body.cpp.erb", __FILE__)
-    @loader = TemplateRenderer.new(File.read(template)).result(@binding)
+    TemplateRenderer.new(File.read(template)).result(binding)
+  end
+
+  def generate_custom_class(name)
+    binding = {project_name:      @binding[:project_name],
+                class_name:        name,
+                member_variables:  [],
+                custom_properties: [],
+    }
+    %w|header body loader|.map{|type|
+      [type, send("generate_#{type}", binding)]
+    }.to_h
   end
 end
