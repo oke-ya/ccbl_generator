26b2968dac83c94330728391e390040d9b4eabd1
yalab <rudeboyjet@gmail.com>
2014-04-03 18:04:46 +0900

    Fixed tests

--- test/unit/ccbl_generator_test.rb
+++ test/unit/ccbl_generator_test.rb
@@ -1,5 +1,12 @@
 require "test_helper"
 
+
+CcblGenerator.module_eval do
+  def get_binding
+    @binding
+  end
+end
+
 class CcblGeneratorTest < Minitest::Unit::TestCase
   def setup
     @ccbl_generator = CcblGenerator.new(project_name: "test",
@@ -7,11 +14,11 @@ class CcblGeneratorTest < Minitest::Unit::TestCase
   end
 
   def test_generate_loader
-    assert_match /__test__MainLayerLoader__/, @ccbl_generator.generate_loader
+    assert_match /__test__MainLayerLoader__/, @ccbl_generator.generate_loader(@ccbl_generator.get_binding)
   end
 
   def test_generate_header
-    header = @ccbl_generator.generate_header
+    header = @ccbl_generator.generate_header(@ccbl_generator.get_binding)
     ['__test__MainLayer__',
      'LabelTTF* title;',
      'std::string myCustomPropertyString;'].each do |expect|
@@ -20,9 +27,10 @@ class CcblGeneratorTest < Minitest::Unit::TestCase
   end
 
   def test_generate_body
-    body = @ccbl_generator.generate_body
+    body = @ccbl_generator.generate_body(@ccbl_generator.get_binding)
     ['if (0 == strcmp(pMemberVariableName, "myCustomProperty"))',
      'else if (0 == strcmp(pMemberVariableName, "myCustomPropertyBool"))'].each do |expect|
+      puts body
       assert_match Regexp.new(Regexp.quote(expect)), body
     end
   end
